77245bf9f0477a7348b049891b4fc863d24996b1
Lau-szutner <lautaroszutner@gmail.com>
2024-08-22 20:38:17 -0300

    agrego svg, css, y funcionaldades del carousel

--- src/components/Fotografia/FotosC.jsx
+++ src/components/Fotografia/FotosC.jsx
@@ -3,7 +3,11 @@ import styled from "./carousel.module.css";
 export default function FotosC({ src }) {
   return (
     <>
-      <img src={src} alt="" className={`${styled.ImagenesComponent}`} />
+      <img
+        src={src}
+        alt=""
+        className={`${styled.ImagenesComponent} ${styled.FOTO}`}
+      />
     </>
   );
 }
--- src/components/Fotografia/MultiItemCarousel.jsx
+++ src/components/Fotografia/MultiItemCarousel.jsx
@@ -2,31 +2,48 @@ import styled from "./carousel.module.css";
 import { ImgLinks } from "../edicion/videos.js";
 import FotosC from "./FotosC.jsx";
 import arrowRight from "../../assets/arrowRight.svg";
+import arrowLeft from "../../assets/arrowLeft.svg";
+
 import { useRef } from "react";
 
 export default function MultiItemCarousel() {
-  const carouselRef = useRef(null);
+  const carouselRefR = useRef(null);
 
-  const handleNext = () => {
-    if (carouselRef.current) {
-      // Mueve el carrusel hacia la derecha
-      carouselRef.current.scrollLeft += carouselRef.current.offsetWidth;
+  const handleNext = (value) => {
+    if (value == 1) {
+      carouselRefR.current.scrollLeft += carouselRefR.current.offsetWidth;
+    } else if (value == 2) {
+      carouselRefR.current.scrollLeft -= carouselRefR.current.offsetWidth;
     }
   };
 
   return (
     <section className="container-fluid my-5">
       <h2 className={styled.h2}>Fotografía</h2>
-      <div className={` ${styled.carousel} `} ref={carouselRef}>
-        {ImgLinks.map((ImgLinks, n) => (
-          <FotosC src={ImgLinks.src} key={n} />
-        ))}
+
+      <div className={`${styled.div1}`}>
+        <div className={` ${styled.carousel} `} ref={carouselRefR}>
+          {ImgLinks.map((ImgLinks, n) => (
+            <FotosC src={ImgLinks.src} key={n} />
+          ))}
+        </div>
+
         <img
+          id={`${styled.btnL}`}
+          src={arrowLeft}
+          alt=""
+          className={`${styled.ArrowRight}`}
+          onClick={() => {
+            handleNext("2");
+          }}
+        />
+        <img
+          id={`${styled.btnR}`}
           src={arrowRight}
           alt=""
           className={`${styled.ArrowRight}`}
           onClick={() => {
-            handleNext();
+            handleNext("1");
           }}
         />
       </div>
