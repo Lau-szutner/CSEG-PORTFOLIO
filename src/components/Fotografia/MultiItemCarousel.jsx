f0bcb810dd0a6c3e3c5c63b2a9e0dc78a195da06
Lau-szutner <lautaroszutner@gmail.com>
2024-08-23 23:58:03 -0300

    terminando carousel

--- src/components/Fotografia/MultiItemCarousel.jsx
+++ src/components/Fotografia/MultiItemCarousel.jsx
@@ -1,17 +1,13 @@
-import styled from "./carousel.module.css";
-import { ImgLinks } from "../edicion/videos.js";
-import FotosC from "./FotosC.jsx";
-import arrowRight from "../../assets/arrowRight.svg";
-import arrowLeft from "../../assets/arrowLeft.svg";
 import { useRef, useState } from "react";
+import styled from "./carousel.module.css";
 
 export default function MultiItemCarousel() {
   const itemsRef = useRef(null);
   const [itemList, setItemList] = useState(setupItemList);
 
-  function scrollToItem(item) {
+  function scrollToItem(cat) {
     const map = getMap();
-    const node = map.get(item);
+    const node = map.get(cat);
     node.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
@@ -27,57 +23,63 @@ export default function MultiItemCarousel() {
     return itemsRef.current;
   }
 
-  // ---------
-
   return (
-    <section className="container-fluid my-5">
-      <h2 className={styled.h2}>Fotografía</h2>
-
-      {/* <div className={`${styled.div1}`}> */}
-      <ul>
-        {itemList.map((item) => (
-          <li
-            key={item}
-            ref={(node) => {
-              const map = getMap();
-              if (node) {
-                map.set(item, node);
-              } else {
-                map.delete(item);
-              }
-            }}
-          >
-            <img src={item} />
-          </li>
-        ))}
-      </ul>
-      <img
-        id={`${styled.btnL}`}
-        src={arrowLeft}
-        alt=""
-        className={`${styled.ArrowRight}`}
-        onClick={() => {
-          scrollToItem(itemList[1]);
-        }}
-      />
-      <img
-        id={`${styled.btnR}`}
-        src={arrowRight}
-        alt=""
-        className={`${styled.ArrowRight}`}
-        onClick={() => {
-          scrollToItem(itemList[5]);
-        }}
-      />
-      {/* </div> */}
-    </section>
+    <>
+      <nav>
+        <button onClick={() => scrollToItem(itemList[0])}>Tom</button>
+        <button onClick={() => scrollToItem(itemList[5])}>Maru</button>
+        <button onClick={() => scrollToItem(itemList[9])}>Jellylorum</button>
+      </nav>
+      <div className={`${styled.carousel}`}>
+        <ul className={`${styled.ul}`}>
+          {itemList.map((cat) => (
+            <li
+              className={`${styled.ImagenesComponent}`}
+              key={cat}
+              ref={(node) => {
+                const map = getMap();
+                if (node) {
+                  map.set(cat, node);
+                } else {
+                  map.delete(cat);
+                }
+              }}
+            >
+              <img src={cat} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
-  function setupItemList() {
-    const itemList = [];
-    for (let i = 0; i < 10; i++) {
-      itemList.push(`${ImgLinks.src}` + i);
-    }
+}
 
-    return itemList;
+function setupItemList() {
+  const itemList = [];
+  for (let i = 0; i < 10; i++) {
+    itemList.push("https://picsum.photos/400/600?random=${i}" + i);
   }
+
+  return itemList;
+}
+
+{
+  /* <img
+          id={`${styled.btnL}`}
+          src={arrowLeft}
+          alt=""
+          className={`${styled.ArrowRight}`}
+          onClick={() => {
+            scrollToItem(itemList[0]);
+          }}
+        />
+        <img
+          id={`${styled.btnR}`}
+          src={arrowRight}
+          alt=""
+          className={`${styled.ArrowRight}`}
+          onClick={() => {
+            scrollToItem(itemList[5]);
+          }}
+        /> */
 }
