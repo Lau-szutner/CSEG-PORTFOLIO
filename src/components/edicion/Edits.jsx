ae17c4d0708e1a03bced920b178fa9e0364e715e
Lau-szutner <lautaroszutner@gmail.com>
2024-08-29 20:50:45 -0300

    eliminacion de comentarios

--- src/components/edicion/Edits.jsx
+++ src/components/edicion/Edits.jsx
@@ -14,5 +14,3 @@ export default function Edits() {
     </section>
   );
 }
-
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/7r7MG_70A0k?si=_axyiHvFvr-Tzia2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
